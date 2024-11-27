66cce09c3f9e6b3a5c2c0840cc8287a6c38bd195
George Zakhary <elprincedoca@gmail.com>
2024-11-27 19:05:45 +0200

    fix profile url

--- jobscanner/urls.py
+++ jobscanner/urls.py
@@ -4,11 +4,11 @@ from jobscanner.views import *
 urlpatterns = [
     path("", index, name="home"),
     path("scanned/", scanned, name="scanned"),
-    path("comment/<int:pk>", comment, name="comment"),
-    path("profile/<int:pk>", profile, name="profile"),
+    path("comment/<uuid:pk>", comment, name="comment"),
+    path("profile/<uuid:pk>", profile, name="profile"),
     # TODO
     path("dashboard/", dashboard, name="dashboard"),
-    path("dashboard/<uuid:pk>", detailed_dashboard, name="detailed_dashboard"),
+    path("dashboard/<int:pk>", detailed_dashboard, name="detailed_dashboard"),
     path("upload/freelancer/", upload_freelancers, name="upload_freelancer"),
     path("upload/recrutier/", upload_recrutiers, name="upload_recrutier"),
 ]
