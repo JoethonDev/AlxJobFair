21d97681607e2baaa920f2e016eb01fd443d4cc2
George Zakhary <elprincedoca@gmail.com>
2024-11-27 15:18:17 +0200

    edit database settings

--- jobfair/settings.py
+++ jobfair/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'jobfair.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alxfusion_jobfair',
+        'USER': 'alxfusion',
+        'PASSWORD': 'alxJobFair2024!',
+        'HOST': 'postgresql-alxfusion.alwaysdata.net',
+        'PORT': '5432'
     }
 }
 
